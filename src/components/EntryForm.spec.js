533ae99d144d7b1419aa88b585c515f73a7e772c
Patrick Müller <p.moe.mueller@gmail.com>
2022-03-16 12:13:07 +0100

    Add old test

--- src/components/EntryForm.spec.js
+++ src/components/EntryForm.spec.js
@@ -3,7 +3,17 @@ import userEvent from '@testing-library/user-event';
 import EntryForm from './EntryForm.js';
 
 describe('EntryForm', () => {
-  it('renders a label and input with placeholder plus a button', () => {
+  // it('renders a label and input with placeholder plus a button', () => {
+  //   render(<EntryForm labelText="add note" placeholder="lean coffee note" />);
+
+  //   const input = screen.getByLabelText('add note');
+  //   expect(input).toHaveAttribute('placeholder', 'lean coffee note');
+
+  //   const submitButton = screen.getByRole('button', { name: '+' });
+  //   expect(submitButton).toBeInTheDocument();
+  // });
+
+  it('renders an input and buttonfunction', () => {
     const callback = jest.fn();
     render(<EntryForm onSubmit={callback} />);
 
